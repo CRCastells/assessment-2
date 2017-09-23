08b006323d58e1a362a8c979babbf0a5d3c7d7a2
Kelrynn <cameroncastells@gmail.com>
2017-09-23 16:54:40 -0600

    commenting code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 $(function() {
 	console.log("JS LOADED");	
 
-
+//creating two objects for the game to load
 let player1 ={
 	car: $('<img>').attr('src', 'assets/blue-car.png'),
 	pos: 88,
 	name: "Player 1",
+	//conditions in both objects for tracking # of wins and if the alternate key needs to be pressed for movement
 	winCount: 0,
 	altKey: false
 
@@ -21,34 +22,45 @@ let player2 ={
 
 } ; 
 
+//removing the cars frin the document flow to allow the movement left -> right
 player1.car.css('position', 'fixed');
 player2.car.css('position', 'fixed');
 
+//adding cars to the body
 $('body').append(player1.car,player2.car);
 
+//positioning the cars into starting position
 player1.car.css('top', '382px');
 player2.car.css('top', '500px');
 player1.car.css('right', '88%');
 player2.car.css('right', '88%');
 
+
+//move function works by taking the object's position property and updating the css of the corresponding car
 function move(obj){
 	obj.pos = obj.pos - 2;
 	obj.car.css('right', obj.pos + "%");
 	hasWon(obj);
 }
 
+//to check if the player car has crossed the finish line (aka right : 0)
 function hasWon(obj) {
+	//the win condition
 	if (obj.pos === 0){
+		//hattrick?
 		if ((obj.winCount+1) % 3 === 0 && obj.winCount != 0){
 			alert(obj.name + " has won a hattrick!");
+			//default win statement
 		} else {
 			alert(obj.name + " has won the race!");
 		}
+		//updating win table
 		if (obj.name === "Player 1"){
 			$('#p1w').html(++player1.winCount);
 		} else {
 			$('#p2w').html(++player2.winCount);
 		}
+		//reseting positions to race again!
 		player1.car.css('right', '88%');
 		player2.car.css('right', '88%');
 		player1.pos = 88;
@@ -56,20 +68,23 @@ function hasWon(obj) {
 
 	}
 }
-
+//event listeners for each car
 $(document).keyup(function(event) {
+	//if altkey is true it requires x to be used instead of x
 	if (player1.altKey){
 		if (event.key === 'x'){
 			move(player1);
 			player1.altKey = false;
 		}
 	}
+	//otherwise you need to press z to move the car
 	else if (event.key === 'z'){
 			move(player1);
 			player1.altKey = true;
 	}
 });
 
+//follows same structure for car 2 except with alt key l and regular key k
 $(document).keyup(function(event) {
 	if (player2.altKey){
 		if (event.key === 'l'){
